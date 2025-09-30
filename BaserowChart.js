8bc74c116cebba96d1c143391bd6aa96588ffbaf
Frederik Duchi <frederikduchi@Frederiks-MacBook-Pro.local>
2025-09-30 14:40:10 +0200

    changed structure to only pass strings instead of elements

--- BaserowChart.js
+++ BaserowChart.js
@@ -1,27 +1,35 @@
 class BaserowChart {
-    constructor() {
-        const title = document.querySelector('title');       
+    constructor(charts_to_render) {
+        this.charts_to_render = charts_to_render;
+
+        const title = document.querySelector('title');
+        const page_title = title.textContent;   
+        this.renderAllCharts(page_title);    
         const observer = new MutationObserver(() => {
-            console.log('Title changed to:', title);
+            console.log('Title changed to:', page_title);
+            renderAllCharts(page_title);
         });
         observer.observe(title, { childList: true });
     }
 
-    renderChart(data_table, column_number, container, chart_type, page_path) {
-        const data = this.get_table_data(data_table,column_number);
-        this.draw_chart(container, chart_type, data, page_path);
-        
+    renderAllCharts(page_title){
+        this.charts_to_render.filter(chart => chart.page_title === page_title).forEach(chart => this.renderChart(chart))
+    }
 
+    renderChart(chart) {
+        const data = this.get_table_data(chart.data_table,chart.column_number);
+        this.draw_chart(document.querySelector(`.${chart.container}`), chart.type, data, chart.page_title);
     }
 
     get_table_data(data_table, column_number) {
-        const labels = Array.from(data_table.querySelectorAll(`tr > td:nth-child(1) .ab-text`)).map(i => i.textContent.trim());
-        const values = Array.from(data_table.querySelectorAll(`tr > td:nth-child(${column_number}) .ab-text`)).map(i => parseFloat(i.textContent));       
-        const title = data_table.querySelector(`thead tr th:nth-child(${column_number})`).textContent.trim();
+        const table = document.querySelector(`.${data_table}`);
+        const labels = Array.from(table.querySelectorAll(`tr > td:nth-child(1) .ab-text`)).map(i => i.textContent.trim());
+        const values = Array.from(table.querySelectorAll(`tr > td:nth-child(${column_number}) .ab-text`)).map(i => parseFloat(i.textContent));       
+        const title = table.querySelector(`thead tr th:nth-child(${column_number})`).textContent.trim();
         return { labels, values,title };
     }
 
-    draw_chart(container, chart_type, data, title) {
+    draw_chart(container, chart_type, data) {
         const ctx = document.createElement('canvas')
         container.appendChild(ctx)
         new Chart(ctx, {
