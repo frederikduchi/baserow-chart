4b1d8b0958679acdebee066f86798f284468e546
Frederik Duchi <frederikduchi@Frederiks-MacBook-Pro.local>
2025-09-30 15:03:34 +0200

    bug fix

--- BaserowChart.js
+++ BaserowChart.js
@@ -3,14 +3,13 @@ class BaserowChart {
         this.charts_to_render = charts_to_render;
 
         const title = document.querySelector('title');
-        
+
         this.renderAllCharts(title.textContent);
 
         const observer = new MutationObserver(() => {
-            
-                const page_title = title.textContent;
-                console.log('Title changed to:', page_title);
-                this.renderAllCharts(page_title);
+            const page_title = title.textContent;
+            console.log('Title changed to:', page_title);
+            this.renderAllCharts(page_title);
         });
         observer.observe(title, { childList: true });
     }
